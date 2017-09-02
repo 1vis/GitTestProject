bf119861b3b3c1d717e44d73a49243ebd4a19afd
Pinsius <l.pinsius@gmail.com>
2017-09-02 22:03:53 +0200

    SelectionBox

--- Source/GitTestProject/Player/GitPlayerController.h
+++ Source/GitTestProject/Player/GitPlayerController.h
@@ -30,6 +30,21 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void HighlightCharacterPanels(TArray<APawn*> SelectedActors);
 
+	UFUNCTION(BlueprintCallable)
+	void StartSelectionMode();
+
+	UFUNCTION(BlueprintCallable)
+	void StopSelectionMode();
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateSelectionBoxData(const float& DeltaTime);
+
+	UFUNCTION(BlueprintCallable)
+	void SetSelectedActors(TArray<APawn*> SelectedActors);
+
+	UFUNCTION(BlueprintCallable)
+	bool IsInSelectionBox(APawn* SelectedActor);
+
 public:
 	UPROPERTY(BlueprintReadWrite, EditAnywhere)
 	class ACameraBase* ActiveCamera = nullptr;	
@@ -39,4 +54,19 @@ public:
 
 	UPROPERTY(BlueprintAssignable)
 	FUpdateThePanelsDelegate UpdateCharacterPanelsFunction;
+
+	UPROPERTY(BlueprintReadWrite)
+	bool SelectBoxMode = false;
+
+	UPROPERTY(BlueprintReadWrite)
+	float SelectionBoxX = 0.0f;
+
+	UPROPERTY(BlueprintReadWrite)
+	float SelectionBoxY = 0.0f;
+
+	UPROPERTY(BlueprintReadWrite)
+	float SelectionBoxW = 0.0f;
+
+	UPROPERTY(BlueprintReadWrite)
+	float SelectionBoxH = 0.0f;
 };
