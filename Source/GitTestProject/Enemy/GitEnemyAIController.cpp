18fc73ef8afb87b976d1c856e6bbcfc0c376bb1e
1vis <ivan.salgovic@gmail.com>
2017-09-05 19:12:49 +0200

    minor

--- Source/GitTestProject/Enemy/GitEnemyAIController.cpp
+++ Source/GitTestProject/Enemy/GitEnemyAIController.cpp
@@ -1,7 +1,34 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "GitEnemyAIController.h"
+#include "Runtime/Engine/Classes/Engine/World.h"
 
 
+void AGitEnemyAIController::Tick(float DeltaTime)
+{
+	for (auto& POI : POIsMemory)
+	{
+		POI.Duration -= DeltaTime;
+	}
+	POIsMemory.RemoveAll([](const FPointOfInterest& CurrentPOI) { return CurrentPOI.Duration <= 0.0f; });
+}
 
+bool AGitEnemyAIController::HasLOS(AActor* ToActor)
+{
+	FHitResult HitResult;
+	FVector Start = GetPawn()->GetActorLocation();
+	FVector End = ToActor->GetActorLocation();
 
+	GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECollisionChannel::ECC_Visibility);
+	return HitResult.bBlockingHit;		
+}
+
+void AGitEnemyAIController::AddPOIToMemory(FVector Location, float Duration/*= 60.0f*/)
+{
+	POIsMemory.Push(FPointOfInterest(Location, Duration));
+}
+
+void AGitEnemyAIController::RemovePOIFromMemory(FVector LocationToRemove, float Tolerance/*= 100.0f*/)
+{
+	POIsMemory.RemoveAll([&](const FPointOfInterest& CurrentPOI){ return CurrentPOI.Location.Equals(LocationToRemove, Tolerance); });
+}
--- Source/GitTestProject/Enemy/GitEnemyAIController.h
+++ Source/GitTestProject/Enemy/GitEnemyAIController.h
@@ -3,8 +3,29 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "AIController.h"
+#include "AIController.h" 
 #include "GitEnemyAIController.generated.h"
+
+USTRUCT()
+struct FPointOfInterest
+{
+	GENERATED_BODY()
+
+	FPointOfInterest()
+	{
+		Location = FVector::ZeroVector;
+		Duration = 0.0f;
+	}
+
+	FPointOfInterest(FVector NewLocation, float NewDuration)
+	{		
+		Location = NewLocation;
+		Duration = NewDuration;
+	}
+
+	FVector Location;
+	float Duration;
+};
 
 /**
  * 
@@ -13,8 +34,22 @@ UCLASS()
 class GITTESTPROJECT_API AGitEnemyAIController : public AAIController
 {
 	GENERATED_BODY()
-	
-	
-	
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintPure)
+	bool HasLOS(AActor* ToActor);
+
+	// @param Duration - How long will Pawn remember this position, in seconds.
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Add POI To Memory"))
+	void AddPOIToMemory(FVector Location, float Duration = 60.0f);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Remove POI From Memory"))
+	void RemovePOIFromMemory(FVector LocationToRemove, float Tolerance = 100.0f);
+
+
+private:
+	UPROPERTY()
+	TArray<FPointOfInterest> POIsMemory;
 	
 };
